db4239e1d40e5e0422ed84a4a67084f552e3a0ca
Roberto Alicata <roberto.alicata@gmail.com>
2018-09-25 16:50:22 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Photo Share Client
+
+- Simple apllication to share photo using React & Apollo Client
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({ uri: `http://localhost:4000/graphql` });
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
